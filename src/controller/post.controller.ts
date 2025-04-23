8064166f096e74990312065067c2d1bc3548f7c5
Sambit99 <sambitkumar75@gmail.com>
2025-04-23 13:04:10 +0530

    feat: Add getPostById function to retrieve a post by its ID

--- src/controller/post.controller.ts
+++ src/controller/post.controller.ts
@@ -4,3 +4,8 @@ export const getAllPosts = async () => {
   const posts = await Post.find();
   return posts;
 };
+
+export const getPostById = async (id: string) => {
+  const posts = await Post.findById(id);
+  return posts;
+};
