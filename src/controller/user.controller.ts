024a61f5d0be92d01c3aa8f769da9e03ca1c01c4
Sambit99 <sambitkumar75@gmail.com>
2025-04-23 13:05:05 +0530

    feat: Add getUserById function to retrieve a user by their ID

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -4,3 +4,9 @@ export const getAllUsers = async () => {
   const users = await User.find();
   return users;
 };
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const getUserById = async (_: any, args: { id: string }) => {
+  const users = await User.findById(args.id);
+  return users;
+};
