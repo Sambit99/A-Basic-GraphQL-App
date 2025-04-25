ef51bd0592c4c13096ca31c0a3c3df6320c9b8c2
Sambit99 <sambitkumar75@gmail.com>
2025-04-25 21:08:39 +0530

    feat: Un-comment and organize imports in app.ts for clarity and structure

--- src/app.ts
+++ src/app.ts
@@ -1,68 +1,74 @@
-// import express, { Application, NextFunction, Request, Response } from 'express';
-// import path from 'path';
-// import helmet from 'helmet';
-// import cors from 'cors';
-// import { StatusCode } from './constant/statusCodes.js';
-// import { ResponseMessage } from './constant/responseMessage.js';
-// import ApiError from './util/ApiError.js';
-
-// import { fileURLToPath } from 'url';
-// import { dirname } from 'path';
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
-import Logger from './util/Logger.js';
-import config from './config/config.js';
-import databaseService from './service/databaseService.js';
+import express, { Application, NextFunction, Request, Response } from 'express';
+import path from 'path';
+import helmet from 'helmet';
+import cors from 'cors';
+import { StatusCode } from './constant/statusCodes.js';
+import { ResponseMessage } from './constant/responseMessage.js';
+import ApiError from './util/ApiError.js';
+import { expressMiddleware } from '@apollo/server/express4';
 import { connectGraphQL } from './graphql/graphql.js';
 
-const port = Number(config.PORT) || 3000;
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-const db = await databaseService.connect();
-Logger.info('DATABASE CONNECTED', { meta: { name: db.name } });
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-const graphQlServer = await connectGraphQL(port);
+const app: Application = express();
 
-Logger.info('Apollo Server Started', {
-  meta: {
-    PORT: port,
-    ENV: config.ENV,
-    SERVER_URL: graphQlServer.url
+const graphQlServer = connectGraphQL();
+await graphQlServer.start();
+
+// Middlewares
+app.use(helmet());
+app.use(
+  cors({
+    origin: ['http://localhost:4000'],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    credentials: true // Note: In Order to allow Cookies
+  })
+);
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+import ApiRouter from './router/api.router.js';
+import globalErrorHandler from './middleware/globalErrorHandler.js';
+
+app.use('/api/v1', ApiRouter);
+app.use('/graphql', expressMiddleware(graphQlServer));
+
+// 404 Error Handler
+app.use((req: Request, _: Response, next: NextFunction) => {
+  try {
+    throw new Error(ResponseMessage.NOT_FOUND('Route'));
+  } catch (error) {
+    ApiError(next, error, req, StatusCode.NOT_FOUND);
   }
 });
 
-// const app: Application = express();
+// Global Error Handler
+app.use(globalErrorHandler);
 
-// Middlewares
-// app.use(helmet());
-// app.use(
-//   cors({
-//     origin: ['http://localhost:4000'],
-//     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-//     credentials: true // Note: In Order to allow Cookies
-//   })
-// );
-// app.use(express.json());
-// app.use(express.static(path.join(__dirname, '..', 'public')));
-// app.use(express.urlencoded({ extended: false }));
+export default app;
 
-// Routes
-// import ApiRouter from './router/api.router.js';
-// import globalErrorHandler from './middleware/globalErrorHandler.js';
+// import Logger from './util/Logger.js';
+// import config from './config/config.js';
+// import databaseService from './service/databaseService.js';
+// import { connectGraphQL } from './graphql/graphql.js';
 
-// app.use('/api/v1', ApiRouter);
+// const port = Number(config.PORT) || 3000;
 
-// 404 Error Handler
-// app.use((req: Request, _: Response, next: NextFunction) => {
-//   try {
-//     throw new Error(ResponseMessage.NOT_FOUND('Route'));
-//   } catch (error) {
-//     ApiError(next, error, req, StatusCode.NOT_FOUND);
+// const db = await databaseService.connect();
+// Logger.info('DATABASE CONNECTED', { meta: { name: db.name } });
+
+// const graphQlServer = await connectGraphQL(port);
+
+// Logger.info('Apollo Server Started', {
+//   meta: {
+//     PORT: port,
+//     ENV: config.ENV,
+//     SERVER_URL: graphQlServer.url
 //   }
 // });
-
-// Global Error Handler
-// app.use(globalErrorHandler);
-
-// export default app;
