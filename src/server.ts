2b224c0b59e5cdd7f2ec9535efa7e91878ca5e11
Sambit99 <sambitkumar75@gmail.com>
2025-04-25 21:09:14 +0530

    feat: Restore and organize server initialization code in server.ts for clarity

--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,45 @@
-// import app from './app.js';
-// import config from './config/config.js';
-// import Logger from './util/Logger.js';
+import app from './app.js';
+import config from './config/config.js';
+import databaseService from './service/databaseService.js';
+import Logger from './util/Logger.js';
 
-// process.on('uncaughtException', (err: Error) => {
-//   Logger.error('UNCAUGHT EXCEPTION! SHUTTING DOWN...', { meta: err });
-//   process.exit(1);
-// });
+process.on('uncaughtException', (err: Error) => {
+  Logger.error('UNCAUGHT EXCEPTION! SHUTTING DOWN...', { meta: err });
+  process.exit(1);
+});
 
-// const PORT = config.PORT || 3000;
+const PORT = config.PORT || 3000;
 
-// const server = app.listen(PORT);
+const server = app.listen(PORT);
 
-// (() => {
-//   try {
-//     // Application Status
-//     Logger.info(`APPLICATION_STARTED`, {
-//       meta: {
-//         PORT: config.PORT,
-//         SERVER_URL: config.SERVER_URL
-//       }
-//     });
-//   } catch (error) {
-//     Logger.error(`APPLICATION_ERROR`, { meta: error });
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+(async () => {
+  try {
+    const db = await databaseService.connect();
+    Logger.info('DATABASE CONNECTED', { meta: { name: db.name } });
 
-//     server.close((error) => {
-//       if (error) {
-//         Logger.error(`APPLICATION_ERROR`, { meta: error });
-//       }
-//       process.exit(1);
-//     });
-//   }
-// })();
+    // Application Status
+    Logger.info(`APPLICATION_STARTED`, {
+      meta: {
+        PORT: config.PORT,
+        SERVER_URL: config.SERVER_URL
+      }
+    });
+  } catch (error) {
+    Logger.error(`APPLICATION_ERROR`, { meta: error });
 
-// process.on('unhandledRejection', (err: Error) => {
-//   Logger.error('UNHANDLER REJECTION! SHUTTING DOWN...', { meta: err });
-//   server.close(() => {
-//     process.exit(1);
-//   });
-// });
+    server.close((error) => {
+      if (error) {
+        Logger.error(`APPLICATION_ERROR`, { meta: error });
+      }
+      process.exit(1);
+    });
+  }
+})();
+
+process.on('unhandledRejection', (err: Error) => {
+  Logger.error('UNHANDLER REJECTION! SHUTTING DOWN...', { meta: err });
+  server.close(() => {
+    process.exit(1);
+  });
+});
