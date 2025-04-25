c2f9cc35bb5fa9a806ac78304d54a1866b0c6997
Sambit99 <sambitkumar75@gmail.com>
2025-04-25 21:10:00 +0530

    feat:  Update connectGraphQL function to be used by Express Server

--- src/graphql/graphql.ts
+++ src/graphql/graphql.ts
@@ -1,17 +1,28 @@
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+// import { startStandaloneServer } from '@apollo/server/standalone';
 import { graphQlSchema } from './schema/schema.js';
 import { graphQlResolver } from './resolvers/resolver.js';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
-export const connectGraphQL = async (port: number) => {
+// export const connectGraphQL = async (port: number) => {
+//   const server = new ApolloServer({
+//     typeDefs: graphQlSchema,
+//     resolvers: graphQlResolver
+//   });
+
+//   const { url } = await startStandaloneServer(server, {
+//     listen: { port }
+//   });
+
+//   return { url };
+// };
+
+export const connectGraphQL = () => {
   const server = new ApolloServer({
     typeDefs: graphQlSchema,
-    resolvers: graphQlResolver
+    resolvers: graphQlResolver,
+    plugins: [ApolloServerPluginLandingPageLocalDefault({ footer: false })]
   });
 
-  const { url } = await startStandaloneServer(server, {
-    listen: { port }
-  });
-
-  return { url };
+  return server;
 };
